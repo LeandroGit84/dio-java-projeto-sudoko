89c28816171c71d2a264714494168cdebdaf04bd
Leandro da Silva Santos <leandrodsilsantos@gmail.com>
2026-03-10 13:23:33 -0300

    Ediçao do README

---
+++ src/br/com/dio/model/Space.java
@@ -0,0 +1,40 @@
+
+package br.com.dio.model;
+
+public class Space {
+    private Integer actual;
+    private final int expected;
+    private final boolean fixed;
+
+    public Space(final int expected, final boolean fixed) {
+        this.expected = expected;
+        this.fixed = fixed;
+
+        if (fixed) {
+            actual = expected;
+        }
+    }
+
+    public Integer getActual() {
+        return this.actual;
+    }
+
+    public void setActual(final Integer actual) {
+        if (fixed) return;
+        this.actual = actual;
+    }
+
+    public void clearSpace() {
+        setActual(null);
+    }
+
+
+    public int getExpected() {
+        return this.expected;
+    }
+
+    public boolean isFixed() {
+        return fixed;
+    }
+
+}
